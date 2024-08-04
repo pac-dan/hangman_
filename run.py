22ada7eb06813badb21a4378ae5bb69d129d686f
pac-dan 1 <djjohnston98@hotmail.com>
2024-08-04 18:54:24 +0000

    import credentials to run.py file

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 import random
 import os 
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from.service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("hangman_words_list")
 
 def main():
     print('Welcome to Hangman!')
