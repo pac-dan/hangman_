d2648e24aecee30064ab1d1d5d6af397c18ae837
pac-dan 1 <djjohnston98@hotmail.com>
2024-08-05 18:50:48 +0000

    test display word and pull words from sheet

--- run.py
+++ run.py
@@ -12,27 +12,27 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("hangman_words_list")
+SHEET = GSPREAD_CLIENT.open("hangman_words_list").sheet1
 
 def get_words_from_sheet():
     # Get all words from sheet 
     words = SHEET.col_values(1)
     return [word.upper() for word in words if word]
 
-def add_word_to_sheet():
+def add_word_to_sheet(word):
     # Add a new word to the sheet
     SHEET.append_row([word_upper()])
 
 class Hangman:
     def __init__(self, words):
         self.words = words
-        self.word_to_guess = random.choice(self, words)
+        self.word_to_guess = random.choice(words)
         self.guesses = set()
         self.incorrect_guesses = 0
-        self.max_guesses = 6
+        self.max_incorrect_guesses = 6
 
-    
-
+    def display_word(self):
+        return ' '.join([letter if letter in self.guesses else '_' for letter in self.word_to_guess])
 
 
 
@@ -42,6 +42,9 @@ class Hangman:
 
 def main():
     print('Welcome to Hangman!')
+    words = get_words_from_sheet()
+    game = Hangman(words)
+    print(game.display_word())
 
 
 if __name__ == "__main__":
