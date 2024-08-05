a069b098beeb3c3e33a4c1a7e11363bd5ab9c55d
pac-dan 1 <djjohnston98@hotmail.com>
2024-08-05 18:38:32 +0000

    get words from sheet, set initial state for hangman class

--- run.py
+++ run.py
@@ -14,8 +14,35 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("hangman_words_list")
 
+def get_words_from_sheet():
+    # Get all words from sheet 
+    words = SHEET.col_values(1)
+    return [word.upper() for word in words if word]
+
+def add_word_to_sheet():
+    # Add a new word to the sheet
+    SHEET.append_row([word_upper()])
+
+class Hangman:
+    def __init__(self, words):
+        self.words = words
+        self.word_to_guess = random.choice(self, words)
+        self.guesses = set()
+        self.incorrect_guesses = 0
+        self.max_guesses = 6
+
+    
+
+
+
+
+
+
+
+
 def main():
     print('Welcome to Hangman!')
+
 
 if __name__ == "__main__":
     main()
